03ea45481170110c2d3d4fdf8df2db20798f9b46
nitinmittal23 <nitinmittal44066@gmail.com>
2020-03-25 17:21:01 +0530

    login with passport.authenticate()

--- Webinar_16/PassportLogin/routes/root.js
+++ Webinar_16/PassportLogin/routes/root.js
@@ -1,4 +1,5 @@
 const route = require('express').Router()
+const passport = require('../passport')
 const Users = require('../db').Users
 
 route.get('/login', (req,res) => {
@@ -8,21 +9,26 @@ route.get('/signup', (req,res)=> {
     res.render('signup')
 }) 
 
-route.post('/login',(req,res)=>{
-    Users.findOne({
-        where: {
-            username: req.body.username,
-        }
-    }).then((user) => {
-        if(!user){
-            return res.send("no such user")
-        }
-        if (user.password !== req.body.password){
-            return res.send("wrong password")
-        }
-        return res.send("hello " + user.firstName)
-    })
-})
+// route.post('/login',(req,res)=>{
+//     Users.findOne({
+//         where: {
+//             username: req.body.username,
+//         }
+//     }).then((user) => {
+//         if(!user){
+//             return res.send("no such user")
+//         }
+//         if (user.password !== req.body.password){
+//             return res.send("wrong password")
+//         }
+//         return res.send("hello " + user.firstName)
+//     })
+// })
+route.post('/login', passport.authenticate('local', {
+    failureRedirect: '/login',
+    successRedirect: '/private'
+}))
+
 
 route.post('/signup', (req,res)=> {
     Users.create ({
